b1e86ed41090468fd95164a19ab42576fcbf89a2
Faheem Shah <faheemshahiiui@gmail.com>
2020-09-30 15:14:44 +0500

    deleted extra assets

--- public/assets/plugins/grapesjs/plugins/NOT IN USE bootstrap/src/blocks/index.js
+++
@@ -1,9 +0,0 @@
-import basics from './basics'
-import components from './components'
-import layout from './layout'
-
-export default (editor, config = {}) => {
-  basics(editor, config)
-  components(editor, config)
-  layout(editor, config)
-}
--- public/assets/plugins/grapesjs/plugins/NOT IN USE bootstrap/src/traits/index.js
+++
@@ -1,5 +0,0 @@
-import selectClass from './selectClass'
-
-export default function (editor, config = {}) {
-  selectClass(editor, config)
-}
